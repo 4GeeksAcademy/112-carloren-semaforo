6c1c7017ead1a6b5f979b1c38f51f2e5185dc121
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-13 11:20:57 +0000

    código optimizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,59 +9,19 @@ let greenLight = false
 
 //create your first component
 const Home = () => {
-	const [red, setRed] = useState("");
-	const [orange, setOrange] = useState("");
-	const [green, setGreen] = useState("");
+	const [color, setColor] = useState("");
 
-	function lightRed() {
-		setOrange("")
-		orangeLight = false
-		setGreen("")
-		greenLight = false
-		if (!redLight) {
-			setRed("0px 0px 35px 20px red")
-			redLight = true
-		} else {
-			setRed("")
-			redLight = false
-		}
-	}
-	function lightOrange() {
-		setRed("")
-		redLight = false
-		setGreen("")
-		greenLight = false
-		if (!orangeLight) {
-			setOrange("0px 0px 35px 20px orange")
-			orangeLight = true
-		} else {
-			setOrange("")
-			orangeLight = false
-		}
-	}
-
-	function lightGreen() {
-		setOrange("")
-		orangeLight = false
-		setRed("")
-		redLight = false
-		if (!greenLight) {
-			setGreen("0px 0px 35px 20px green")
-			greenLight = true
-		} else {
-			setGreen("")
-			greenLight = false
-		}
-	}
-
+	const red = "0px 0px 35px 20px red"
+	const orange = "0px 0px 35px 20px orange"
+	const green = "0px 0px 35px 20px green"
 
 	return (
 		<div className="d-flex flex-column text-center">
 			<p className="bg-dark m-0 p-2 align-self-center" style={{ width: "10px", height: "50px" }}> </p>
 			<div className="badge bg-dark m-0 p-2 align-self-center">
-				<p className="bg-danger m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: red }} onClick={lightRed}></p>
-				<p className="bg-warning m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: orange }} onClick={lightOrange}></p>
-				<p className="bg-success m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: green }} onClick={lightGreen}></p>
+				<p className="bg-danger m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: (color === red ? red : "") }} onClick={() => (color === red ? setColor("") : setColor(red))}></p>
+				<p className="bg-warning m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: (color === orange ? orange : "") }} onClick={() => (color === orange ? setColor("") : setColor(orange))}></p>
+				<p className="bg-success m-1 rounded-circle" style={{ width: "50px", height: "50px", WebkitBoxShadow: (color === green ? green : "") }} onClick={() => (color === green ? setColor("") : setColor(green))}></p>
 			</div>
 		</div >
 	);
